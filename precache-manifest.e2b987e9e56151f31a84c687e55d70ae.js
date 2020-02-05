a39f0e947feeec34c1072936607fcf68974c6a29
Patrick <patrick.yeung7@gmail.com>
2020-02-04 21:40:53 -0500

    Updates

---
+++ precache-manifest.e2b987e9e56151f31a84c687e55d70ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8860b19b3ebf1413267b92f73b5f9480",
+    "url": "/react-clicky/index.html"
+  },
+  {
+    "revision": "1faf727d59e6b7741ec7",
+    "url": "/react-clicky/static/css/main.06e4b898.chunk.css"
+  },
+  {
+    "revision": "58b43dd7b7be59e0ec08",
+    "url": "/react-clicky/static/js/2.0479c98a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-clicky/static/js/2.0479c98a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1faf727d59e6b7741ec7",
+    "url": "/react-clicky/static/js/main.22a4a449.chunk.js"
+  },
+  {
+    "revision": "c7d90f3806589510eae5",
+    "url": "/react-clicky/static/js/runtime-main.8acab4b3.js"
+  }
+]);
